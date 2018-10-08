794fc8d8e0c72768d31f6dc7826c488f1caf7c18
Hamid <h.habibullah@student.fontys.nl>
2018-10-08 09:58:07 +0200

    Scanner-ble

---
+++ Scanner.py
@@ -0,0 +1,32 @@
+from bluepy.btle import Scanner, DefaultDelegate
+
+class scanner:
+
+    def __init__(self,_,_,_):
+        pass
+    super.__init__()
+
+
+
+    def scan(self): # scan for ble devices
+        pass
+
+
+    def clear(self):  # clear all the current discovered devices
+        pass
+
+    def start(self):  #  make it enable to start receving broadcast from the peripherals
+        pass
+
+    def stop(self):   #  Disable the reception of broadcast
+        pass
+
+    def getDevices(self):  # Return the list of all devices which have been discovered.
+        pass
+
+
+
+# constructor for creation and initialization of a new scanner object
+scan = scanner(arguments)
+
+
